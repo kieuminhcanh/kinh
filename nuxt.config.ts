15337ec1ab3051ca5980fe565008155fab48a626
Ken Kieu <minhcanhdn0704@gmail.com>
2024-03-17 17:05:28 -0700

    Add PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 import fs from 'fs'
 const baseURL = '/kinh'
+const isProduction = process.env.NODE_ENV === 'production'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
-    baseURL: process.env.NODE_ENV === 'production' ? `${baseURL}/` : '/',
+    baseURL: isProduction ? `${baseURL}/` : '/',
     head: {
       title: 'Kinh Phật',
       link: [{ rel: 'icon', type: 'image/png', href: `${baseURL}/logo.png` }],
@@ -14,9 +16,11 @@ export default defineNuxtConfig({
         { hid: 'og:image', name: 'og:image', content: 'https://kieuminhcanh.github.io/kinh/images/kinh-dieu-phap-lien-hoa.jpg' },
       ],
     },
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' },
   },
   ssr: false,
-  modules: ['@nuxt/content', 'vuetify-nuxt-module'],
+  modules: ['@nuxt/content', 'vuetify-nuxt-module', '@vite-pwa/nuxt'],
   content: {
     documentDriven: true,
     // https://content.nuxtjs.org/api/configuration
@@ -52,8 +56,49 @@ export default defineNuxtConfig({
   },
   router: {
     options: {
-      scrollBehaviorType: 'smooth'
-    }
+      scrollBehaviorType: 'smooth',
+    },
+  },
+  pwa: {
+    // strategies: isProduction ? 'injectManifest' : 'generateSW',
+    // srcDir: isProduction ? 'service-worker' : undefined,
+    // filename: isProduction ? 'sw.ts' : undefined,
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Kinh',
+      short_name: 'Kinh',
+      theme_color: '#ffffff',
+      scope: '/kinh/',
+      start_url: '/kinh/',
+      icons: [
+        {
+          src: 'logo-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'logo-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    client: {
+      installPrompt: true,
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallback: '/',
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
   },
   css: ['~/assets/css/main.scss'],
 })
